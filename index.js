8288e8232f41d0dcaee8070469eda415a90c037b
sreeharim <sreeharim1992@gmail.com>
2017-07-23 18:50:57 +0530

    Adding multiple player support

--- index.js
+++ index.js
@@ -9,33 +9,60 @@ app.use(express.static(__dirname+"/public"));
 io.sockets.on('connection',function(socket){
 socket.on('register',function(data){
 	console.log('Registration started');
-	var newUser = {};
-	if(users.length == 0){
-		socket.value=0;
-		socket.turn = true;
-		newUser.val = socket.value;
-		newUser.turn = socket.turn; 
-		newUser.socket = socket;
-		users.push(newUser)
+	var newUser = {};//new user
+	var foundPair = false;
+	//default values
+	socket.value = 'X';
+	socket.turn = true;
+	socket.isPaired = false;
+	socket.name = data;
+	socket.pairName = '';
+	socket.pairIndex = -1;
+	socket.coins = 100;
+	socket.pairCoins = -1;
+
+	console.log('No of Users:'+users.length);
+	for(var i in users){
+	 	    console.log('User at index '+i+' is paired :'+users[i].socket.isPaired);                // checking for unpaired users 
+		if(!(users[i].socket.isPaired)){            //found unpaired user
+			console.log('Found pair at index:'+i);
+			if(users[i].socket.value == 'X'){ //setting up new user value based on pair value
+				socket.value = 'O';
+				socket.turn = false;
+				users[i].socket.turn = true;
+			} 
+				
+			socket.isPaired = true;
+			socket.name = data;
+			socket.pairName = users[i].socket.name;
+			socket.pairIndex = i;
+			socket.pairCoins = users[i].socket.coins;
+			users[i].socket.pairName = data; 
+			users[i].socket.pairIndex = users.length;
+			users[i].socket.isPaired = true; 		
+			users[i].socket.emit('opponent connected',{'turn':users[i].socket.turn,'pair':socket.name,'pairCoins':socket.coins});
+			break;
+
+		}
 	}
-	else if(users.length == 1){
-		socket.value=1;
-		socket.turn = false;
-		newUser.val = socket.value;
-		newUser.turn = socket.turn;
-		newUser.socket = socket;
-		users.push(newUser); 
-	}
-		
-	socket.emit('registered user',{'val':newUser.val,'turn':newUser.turn});
+	
+	newUser.socket = socket;
+	users.push(newUser); 
+	
+	var registeredUser = {
+						  'val':newUser.socket.value,
+						  'turn':newUser.socket.turn,
+						  'pair':newUser.socket.pairName,
+						  'coins':newUser.socket.coins,
+						  'pairCoins':newUser.socket.pairCoins
+						  };	
+	socket.emit('registered user',registeredUser);
 });
 
  socket.on('submit move',function(data){
  	socket.emit('your turn',false);
- 	if(socket.value == 0)
- 		users[1].socket.emit('your turn',true);
- 	if(socket.value == 0)
- 		users[0].socket.emit('your turn',true);
- 	io.sockets.emit('update board',data);
+ 	users[socket.pairIndex].socket.emit('your turn',true);
+ 	socket.emit('update board',data);
+ 	users[socket.pairIndex].socket.emit('update board',data);
  });
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,68 @@
 var app = angular.module("tttApp",[])
 				.controller("tttController",function($scope){
-					var gameBoard = [{zero:-1,one:-1,two:-1,id:"r1"},
-									 {zero:-1,one:-1,two:-1,id:"r2"},
-									 {zero:-1,one:-1,two:-1,id:"r3"}
+					var gameBoard = [{zero:'',one:'',two:'',id:'r1'},
+									 {zero:'',one:'',two:'',id:'r2'},
+									 {zero:'',one:'',two:'',id:'r3'}
 									];
 					$scope.gameBoard = gameBoard;
 					$scope.isMyTurn = false;
+					$scope.loggedIn = false;
+					$scope.validMove =false;
+					$scope.message = "Waiting for an opponent...";
 					$scope.socket = io.connect();
-					$scope.socket.emit('register','register');
+					$scope.name="";
+					$scope.opponent=""; 
+					$scope.coins=-1;
+					$scope.opponentCoins=-1;
 					$scope.socket.on('registered user',function(data){
 								console.log(data);
 								$scope.value = data.val;
+								$scope.coins = data.coins;
+								$scope.opponent=data.pair;
+								$scope.opponentCoins=data.pairCoins;
 								$scope.isMyTurn = data.turn;
+								if($scope.opponent != '')
+									 $scope.message = "Oppponent connected";
+								$scope.$apply();
 								});
 					$scope.socket.on('update board',function(data){
 								$scope.gameBoard = data;
 								 $scope.$apply();
 								});
+					
+					$scope.socket.on('opponent connected',function(data){
+								 $scope.opponent=data.pair;
+								 $scope.opponentCoins=data.pairCoins;
+								 $scope.isMyTurn = data.turn;
+								 $scope.message = "Oppponent connected"
+								 $scope.$apply();
+								});
+
 					$scope.socket.on('your turn',function(data){
+								console.log('Your turn:' + data);
 								$scope.isMyTurn = data;
-								 //$scope.$apply();
+								 $scope.$apply();
 								});
 						
 					$scope.submitChange = function(row,key) {
+							$scope.validMove = false;
         					$scope.gameBoard = $scope.updateGameBoard($scope.gameBoard,row,key,$scope.value);
-							$scope.socket.emit('submit move',$scope.gameBoard);
+        					if($scope.validMove)
+								$scope.socket.emit('submit move',$scope.gameBoard);
     						}
     				$scope.updateGameBoard = function(gameBoard, row, key,val){
     					for(var gameRow in gameBoard)
-							if(gameBoard[gameRow].id == row && gameBoard[gameRow][key] == -1){
+							if(gameBoard[gameRow].id == row && gameBoard[gameRow][key] == ''){
 								gameBoard[gameRow][key] = val;
+								$scope.validMove = true;
 								break;
 								}
 							return gameBoard;	
+    				}
+    				$scope.enterGame = function(){
+    					console.log("In game...");
+    					$scope.loggedIn = true;
+    					$scope.socket.emit('register',$scope.nickname);
     				}		
 
 				});

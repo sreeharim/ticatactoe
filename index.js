5b6ad2d22ece99fbd727293a6c1d1da3c959fa26
sreeharim <sreeharim1992@gmail.com>
2017-07-25 02:17:55 +0530

    Handling disconnect users

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ app.use(express.static(__dirname+"/public"));
 
 io.sockets.on('connection',function(socket){
 socket.on('register',function(data){
-	console.log('Registration started for user..'+data);
+	//checking for duplicate username..
+	var validName = true;
+	for(var i in users){
+		if(data == users[i].socket.name){
+			validName = false;
+			socket.emit("invalid name","Nickname already taken...try another");
+			break;
+		}
+	}
+	if(validName){
+		console.log('Registration started for user..'+data);
 	var newUser = {};//new user
 	var foundPair = false;
 	//default values
@@ -21,48 +31,81 @@ socket.on('register',function(data){
 	socket.coins = 100;
 	socket.pairCoins = '';
 
-	console.log('No of Users:'+users.length);
-	for(var i in users){
-	 	    console.log('User at index '+i+' is paired :'+users[i].socket.isPaired);                // checking for unpaired users 
-		if(!(users[i].socket.isPaired)){            //found unpaired user
-			console.log('Found pair at index:'+i);
-			if(users[i].socket.value == 'X'){ //setting up new user value based on pair value
-				socket.value = 'O';
-				socket.turn = false;
-				users[i].socket.turn = true;
-			} 
-				
-			socket.isPaired = true;
-			socket.name = data;
-			socket.pairName = users[i].socket.name;
-			socket.pairIndex = i;
-			socket.pairCoins = users[i].socket.coins;
-			users[i].socket.pairName = data; 
-			users[i].socket.pairIndex = users.length;
-			users[i].socket.isPaired = true; 		
-			users[i].socket.emit('opponent connected',{'turn':users[i].socket.turn,'pair':socket.name,'pairCoins':socket.coins});
-			break;
-
-		}
-	}
-	
+	newGame(data);
 	newUser.socket = socket;
 	users.push(newUser); 
+	socket.emit('registered user','success');
 	
-	var registeredUser = {
-						  'val':newUser.socket.value,
-						  'turn':newUser.socket.turn,
-						  'pair':newUser.socket.pairName,
-						  'coins':newUser.socket.coins,
-						  'pairCoins':newUser.socket.pairCoins
-						  };	
-	socket.emit('registered user',registeredUser);
+	}
+	
 });
-
+ //on a successfull move update the board
  socket.on('submit move',function(data){
+ 	var pairSocket = findPairSocket(socket.pairName);
  	socket.emit('your turn',false);
- 	users[socket.pairIndex].socket.emit('your turn',true);
+ 	pairSocket.emit('your turn',true);
  	socket.emit('update board',data);
- 	users[socket.pairIndex].socket.emit('update board',data);
+ 	pairSocket.emit('update board',data);
  });
+ //on newGame request find a free user
+ socket.on("new game",function(data){
+ 	newGame(socket.name);	
+ });
+ //on disconnect remove user and inform opponent
+ socket.on('disconnect',function(data){
+ 	console.log('Disconnecting user....'+socket.name);
+ 	var pairSocket = findPairSocket(socket.pairName);
+ 	if(pairSocket){
+ 		pairSocket.pairName="";
+ 		pairSocket.isPaired = false;
+ 		pairSocket.emit('opponent disconnected','Opponent disconnected');	
+ 	}
+ 	index = getUserIndex(socket.name)
+ 	users.splice(index,1);
+ });
+
+function newGame(name){
+	var upSocket = getUnpairedSocket(name);
+	if(upSocket){
+		if(upSocket.value == 'X'){ //setting up new user value based on pair value
+			socket.value = 'O';
+			socket.turn = false;
+			upSocket.turn = true;
+			}
+		else{
+			socket.value = 'X';
+			socket.turn = true;
+			upSocket.turn = false;
+		} 
+			socket.isPaired = true;
+			socket.name = name;
+			socket.pairName = upSocket.name;
+			socket.pairCoins = upSocket.coins;
+			upSocket.pairName = name; 
+			upSocket.isPaired = true; 		
+			upSocket.emit('opponent connected',{'val':upSocket.value,'turn':upSocket.turn,'coins':upSocket.coins,'pair':socket.name,'pairCoins':socket.coins});
+			socket.emit('opponent connected',{'val':socket.value,'turn':socket.turn,'coins':socket.coins,'pair':socket.pairName,'pairCoins':socket.pairCoins});
+	}
+}
+
+function getUserIndex(name){
+	for(var i in users)
+		if(users[i].socket.name == name)
+			return i;
+	
+}
+
+function getUnpairedSocket(name){
+	for(var i in users){
+		if(!(users[i].socket.isPaired) && users[i].socket.name != name)
+			return users[i].socket;
+	}
+}
+
+function findPairSocket(name){
+	for(var i in users)
+		if(users[i].socket.name == name)
+			return users[i].socket;
+	return null;
+}
 });
--- public/js/app.js
+++ public/js/app.js
@@ -12,21 +12,14 @@ var app = angular.module("tttApp",[])
 					$scope.socket = io.connect();
 					$scope.name="";
 					$scope.opponentConnected = false;
+					$scope.opponentDisConnected = false;
 					$scope.opponent=""; 
 					$scope.coins='';
 					$scope.opponentCoins='';
+					$scope.invalidUsrMsg= '';
 					$scope.socket.on('registered user',function(data){
 								console.log(data);
-								$scope.value = data.val;
-								$scope.coins = data.coins;
-								$scope.opponent=data.pair;
-								$scope.opponentCoins=data.pairCoins;
-								$scope.isMyTurn = data.turn;
-								if($scope.opponent != ''){
-									$scope.opponentConnected = true;
-									$scope.message = "Oppponent connected";
-								}
-									 
+								$scope.loggedIn = true;
 								$scope.$apply();
 								});
 					$scope.socket.on('update board',function(data){
@@ -37,19 +30,33 @@ var app = angular.module("tttApp",[])
 					$scope.socket.on('opponent connected',function(data){
 								 console.log(data);
 								 $scope.opponentConnected = true;
+								 $scope.opponentDisConnected = false;
 								 $scope.opponent = data.pair;
 								 $scope.opponentCoins=data.pairCoins;
 								 $scope.isMyTurn = data.turn;
+								 $scope.value = data.val;
+								 $scope.coins = data.coins;
 								 $scope.message = "Oppponent connected"
 								 $scope.$apply();
 								});
+					$scope.socket.on('opponent disconnected',function(data){
+								$scope.isMyTurn = false;
+								$scope.opponentConnected = false;
+								$scope.opponentDisConnected = true;
+								$scope.message= data;
+								$scope.$apply();
+					});
 
 					$scope.socket.on('your turn',function(data){
 								console.log('Your turn:' + data);
 								$scope.isMyTurn = data;
 								 $scope.$apply();
 								});
-						
+					$scope.socket.on('invalid name',function(data){
+						$scope.invalidUsrMsg=data;
+						$scope.name='';
+						$scope.$apply();
+					});	
 					$scope.submitChange = function(row,key) {
 							$scope.validMove = false;
         					$scope.gameBoard = $scope.updateGameBoard($scope.gameBoard,row,key,$scope.value);
@@ -66,9 +73,17 @@ var app = angular.module("tttApp",[])
 							return gameBoard;	
     				}
     				$scope.enterGame = function(){
-    					console.log("In game...");
-    					$scope.loggedIn = true;
     					$scope.socket.emit('register',$scope.name);
-    				}		
+    				}
+    				$scope.clearErr = function(){
+    					$scope.invalidUsrMsg='';
+    				}
+    				$scope.newGame = function(){
+    					$scope.gameBoard =gameBoard;
+    					$scope.socket.emit('new game','');
+    					$scope.message = "Waiting for an opponent...";
+    					$scope.opponentConnected = false;
+						$scope.opponentDisConnected = false;	
+    				}
 
 				});

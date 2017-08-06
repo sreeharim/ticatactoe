63c1e572ace6a3dc62dce2e89779378bce947fe4
sreeharim <sreeharim1992@gmail.com>
2017-08-07 00:19:35 +0530

    separating client code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var users = [];
-server.listen(3000);
+server.listen(process.env.PORT ||3000);
 app.use(express.static(__dirname+"/public"));
 
 io.sockets.on('connection',function(socket){
